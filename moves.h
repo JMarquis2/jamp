653673912c73fc365d8f30d1d5254974541d1e49
unknown <marti1pv@gmail.com>
2023-06-06 17:49:14 -0400

    created enemy class and movement towards player/entities

--- moves.h
+++ moves.h
@@ -4,6 +4,7 @@
 #include "Obstacle.h"
 std::vector<int> angleToCardinals(float angle);
 float cardinalsToAngle(int* cardinals);
+float entityToEntityAngle(sf::Vector2f entity, sf::Vector2f Player);
 
 //bool movesWithCollision(sf::Shape* mover, float* moveInfo, sf::Time* elapsed, std::list<Obstacle*>* obstacles, sf::RenderWindow* window);
 //bool movesWithCollision(sf::Sprite* mover, float* moveInfo, sf::Time* elapsed, std::list<Obstacle*>* obstacles, sf::RenderWindow* window);
